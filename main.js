20f432200bf1fbb2a58e1d82a82ee9704915f858
Gabriele Souza <gabriele.ss@icloud.com>
2022-04-12 14:50:17 -0300

    confirmar em main

--- main.js
+++ main.js
@@ -1,45 +1,16 @@
-function teclaPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
-document.querySelector('.tecla_pom').onclick = teclaPom;
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
 
-function teclaClap(){
-    document.querySelector('#som_tecla_clap').play();
+while (contador < listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
+    contador +=1;
 }
-document.querySelector('.tecla_clap').onclick = teclaClap;
-
-function teclaTim(){
-    document.querySelector('#som_tecla_tim').play();
-}
-document.querySelector('.tecla_tim').onclick = teclaTim;
-
-function teclaPuff(){
-    document.querySelector('#som_tecla_puff').play();
-}
-document.querySelector('.tecla_puff').onclick = teclaPuff;
-
-function teclaSplash(){
-    document.querySelector('#som_tecla_splash').play();
-}
-document.querySelector('.tecla_splash').onclick = teclaSplash;
-
-function teclaToim(){
-    document.querySelector('#som_tecla_toim').play();
-}
-document.querySelector('.tecla_toim').onclick = teclaToim;
-
-function teclaPsh(){
-    document.querySelector('#som_tecla_psh').play();
-}
-document.querySelector('.tecla_psh').onclick = teclaPsh;
-
-function teclaTic(){
-    document.querySelector('#som_tecla_tic').play();
-}
-document.querySelector('.tecla_tic').onclick = teclaTic;
-
-function teclaTom(){
-    document.querySelector('#som_tecla_tom').play();
-}
-document.querySelector('.tecla_tom').onclick = teclaTom;
