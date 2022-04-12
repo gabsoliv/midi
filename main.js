b73b806eda6579533c5a003fc77c21b9ce35ba27
Gabriele Souza <gabriele.ss@icloud.com>
2022-04-12 17:33:30 -0300

    melhorando o código

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio){
+   const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido.');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
+
     tecla.onclick = function(){
         tocaSom(idAudio);
-    };
-    contador +=1;
+    };  
+
+    tecla.onkeydown = function(evento){
+
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+      
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
