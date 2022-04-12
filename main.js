a149fb950acba2bd23c58d14e9b072c6b5fe4ab4
Gabriele Souza <gabriele.ss@icloud.com>
2022-04-11 23:33:38 -0300

    MIDI

---
+++ main.js
@@ -0,0 +1,45 @@
+function teclaPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = teclaPom;
+
+
+function teclaClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = teclaClap;
+
+function teclaTim(){
+    document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclick = teclaTim;
+
+function teclaPuff(){
+    document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff').onclick = teclaPuff;
+
+function teclaSplash(){
+    document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash').onclick = teclaSplash;
+
+function teclaToim(){
+    document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector('.tecla_toim').onclick = teclaToim;
+
+function teclaPsh(){
+    document.querySelector('#som_tecla_psh').play();
+}
+document.querySelector('.tecla_psh').onclick = teclaPsh;
+
+function teclaTic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tic').onclick = teclaTic;
+
+function teclaTom(){
+    document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector('.tecla_tom').onclick = teclaTom;
